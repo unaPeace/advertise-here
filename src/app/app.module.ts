defa0d13055ac757a5e539cf5f9cbad5721b0eb7
unaPeace <84766354+unaPeace@users.noreply.github.com>
2022-07-29 15:08:30 +0200

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { SignageComponent } from './views/signage/signage.component';
+import { BrandingComponent } from './views/branding/branding.component';
+import { DigitalComponent } from './views/digital/digital.component';
+import { CaseStudyComponent } from './views/case-study/case-study.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignageComponent,
+    BrandingComponent,
+    DigitalComponent,
+    CaseStudyComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/signage/signage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signage',
+  templateUrl: './signage.component.html',
+  styleUrls: ['./signage.component.scss']
+})
+export class SignageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
