f51650efc0b2872501c8904ecb6b9dc323467c7c
unaPeace <84766354+unaPeace@users.noreply.github.com>
2022-08-03 11:49:31 +0200

    Here we go baby!!!!!!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBu58nTx_7Zg_FWUX3XXaP42Pog4RIdcXY",
+    authDomain: "advertise-here-bd0bf.firebaseapp.com",
+    databaseURL: "https://advertise-here-bd0bf-default-rtdb.firebaseio.com",
+    projectId: "advertise-here-bd0bf",
+    storageBucket: "advertise-here-bd0bf.appspot.com",
+    messagingSenderId: "56271848858",
+    appId: "1:56271848858:web:6bd1acdc8543f49c1a2623",
+    measurementId: "G-ZDY582H837"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBu58nTx_7Zg_FWUX3XXaP42Pog4RIdcXY",
+    authDomain: "advertise-here-bd0bf.firebaseapp.com",
+    databaseURL: "https://advertise-here-bd0bf-default-rtdb.firebaseio.com",
+    projectId: "advertise-here-bd0bf",
+    storageBucket: "advertise-here-bd0bf.appspot.com",
+    messagingSenderId: "56271848858",
+    appId: "1:56271848858:web:6bd1acdc8543f49c1a2623",
+    measurementId: "G-ZDY582H837"
+  },
 };
 
 /*
