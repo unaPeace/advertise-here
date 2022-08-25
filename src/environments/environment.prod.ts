6918ca339f1e20ecfff84a8c7fce7252544f77ff
unaPeace <84766354+unaPeace@users.noreply.github.com>
2022-08-25 15:55:42 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,4 +10,7 @@ export const environment = {
     appId: "1:56271848858:web:6bd1acdc8543f49c1a2623",
     measurementId: "G-ZDY582H837"
   },
+  spaceId: 'k65w7jpu2orq',
+  accessToken: '5_M_5ilaqgdH8k4xd23BT-ui8ExbEEQS5x80i0YYXAg',
+  contentType: 'caseStudy',
 };
